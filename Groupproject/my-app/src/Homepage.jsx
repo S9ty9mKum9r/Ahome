81adb00f6cb3a1c513899280df3dd131bfd7a69d
AmiyaSharma <yash7773@gmail.com>
2023-12-13 18:49:27 +0530

    com

--- Groupproject/my-app/src/Homepage.jsx
+++ Groupproject/my-app/src/Homepage.jsx
@@ -1,8 +1,62 @@
+
 import "./CSSPages/Navbar.css"
 import  img from "./Images/b.jpg"
 import "./CSSPages/Footer.css"
 import  imga from "./Images/c.jpg"
 import  imag from "./Images/d.jpg"
+import React, { useState, useEffect } from 'react';
+
+
+const MyComponent = () => {
+  const [data, setData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const api = "https://63c9b142320a0c4c954daee6.mockapi.io/products";
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(api);
+      const realdata = await response.json();
+      console.log(realdata);
+      setData(realdata);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    
+  }, []);
+
+
+
+  return (
+    
+   <>
+   
+   <div>
+
+
+   </div>
+     <div className="container">
+      {/* Render your component using the 'data' state */}
+      {data.map(item => (
+        <div className="item">
+       <img key={item.id} src={item.image} alt="" />
+            <h3>Name:- {item.name}</h3>
+            <p>Discription:- {item.discription}</p></div>
+
+
+      
+
+       
+
+      ))}
+    </div>
+   </>
+  );
+};
 
 
 
@@ -75,7 +129,9 @@ function Footer(){
 function Homepage(){
     return (
    <>
+  
          <div><Navbar/></div>
+         <MyComponent/>
          <div><Footer/></div>
    </>
     )
