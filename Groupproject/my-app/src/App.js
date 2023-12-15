805a3f11942021fd3fa18499b13d7fe3f6f518c4
AmiyaSharma <yash7773@gmail.com>
2023-12-15 17:43:05 +0530

    complete

--- Groupproject/my-app/src/App.js
+++ Groupproject/my-app/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 // import Landing from './Landing'
 // import LogInPage from './LogInPage'
 // import Rajistration from './Rajistration'
-import MenuPage from './MenuPage';
-// import Homepage from './Homepage';
+// import MenuPage from './MenuPage';
+import Homepage from './Homepage';
 function App() {
   return (
     <div>
       {/* <Landi/ng/> */}
       {/* <LogInPage/> */}
      {/* <Rajistration/> */}
-     {/* <Homepage/> */}
-     <MenuPage/>
+     <Homepage/>
+     {/* <MenuPage/> */}
     </div>
   )
 }
--- Groupproject/my-app/src/Navbar.jsx
+++ Groupproject/my-app/src/Navbar.jsx
@@ -1,26 +1,54 @@
-import  img from "./Images/b.jpg"
+import React, { useState } from 'react';
+import img from "./Images/b.jpg";
 
-function Navbar(){
-    return(<>
-        <div className="Nav">
-     <div id="left">   <button id="img"><img src={img} alt="error" /></button>
-<div className="Menuside">
+function Navbar({ onSearch }) {
+  const [searchInput, setSearchInput] = useState('');
 
+  const handleSearch = (event) => {
+    const inputValue = event.target.value;
+    setSearchInput(inputValue);
+    onSearch(inputValue);
+  };
 
-<button>Blog</button>
-<button>favorite</button>
-</div></div>
-<div className="input">
-    <input type="Value" placeholder="      Search your dish"/>
-    <button>Search</button>
-</div>
-<div className="logindiv">
-    <button>Login</button>
-    <button><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M1 4h14v10a2 2 0 01-2 2H3a2 2 0 01-2-2V4zm7-2.5A2.5 2.5 0 005.5 4h-1a3.5 3.5 0 117 0h-1A2.5 2.5 0 008 1.5z"></path></svg></button>
-</div>        
-
+  return (
+    <div className="Nav">
+      <div id="left">
+        <button id="img">
+          <img src={img} alt="error" />
+        </button>
+        <div className="Menuside">
+          <button>Blog</button>
+          <button>Favorite</button>
+          <button>Menu</button>
         </div>
-    </>)
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Search your Dish"
+          value={searchInput}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="logindiv">
+ 
+        <button>Login</button>
+        <button>
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1 4h14v10a2 2 0 01-2 2H3a2 2 0 01-2-2V4zm7-2.5A2.5 2.5 0 005.5 4h-1a3.5 3.5 0 117 0h-1A2.5 2.5 0 008 1.5z"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
